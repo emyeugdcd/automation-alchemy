c11c57f37d9e0f4e84768f9ecc20f31566e70d38
emyeugdcd <lducanh1991@gmail.com>
2026-05-11 21:53:07 +0300

    bugs fixed and test file added

--- super_deploy.sh
+++ super_deploy.sh
@@ -5,7 +5,7 @@ echo "Booting 6-Node Infrastructure..."
 vagrant up
 
 echo "Installing Core Infrastructure..."
-ansible-playbook -i inventory.ini setup.yml -e "ansible_user=vagrant devops_password=SuperSecurePassword123!" || ansible-playbook -i inventory.ini setup.yml -e "ansible_user=devops devops_password=SuperSecurePassword123!"
+ansible-playbook -i inventory.ini setup.yml -e "ansible_user=vagrant devops_password=SuperSecurePassword123! ansible_become_pass=SuperSecurePassword123!" || ansible-playbook -i inventory.ini setup.yml -e "ansible_user=devops devops_password=SuperSecurePassword123! ansible_become_pass=SuperSecurePassword123!"
 
 echo "Deployment Scripts Executed!"
 echo "The GitHub Actions CI/CD will now handle the application deployment when code is pushed!"
